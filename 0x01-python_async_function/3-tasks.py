e50d24f1f6bbc8162d2bd9185a5f6070717342a9
koredeycode <koredey4u@gmail.com>
2023-05-02 23:56:43 +0000

    test

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,3 +2,12 @@
 """
 documentation for this module
 """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+        Function documentation
+    """
+    return asyncio.Task(wait_random(max_delay))
