4a2804b1291ebd63140614fea13058434ad035c2
koredeycode <koredey4u@gmail.com>
2023-05-02 23:51:28 +0000

    lfg

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,3 +2,15 @@
 """
 documentation for this module
 """
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+        Function documentation
+    """
+    start: float = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.perf_counter() - start) / n
