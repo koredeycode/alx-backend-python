18c74812bd610b183947757864408a5ced47f77b
koredeycode <koredey4u@gmail.com>
2023-05-03 00:06:40 +0000

    done

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,3 +2,14 @@
 """
 documentation for this module
 """
+import asyncio
+from typing import List
+twr = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        function documentation
+    """
+    res = await asyncio.gather(*[twr(max_delay) for _ in range(n)])
+    return sorted(res)
