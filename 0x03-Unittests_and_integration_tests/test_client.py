4d79200ea63fefd9100a86f4c6087414a05a9449
koredeycode <koredey4u@gmail.com>
2023-07-31 13:37:58 +0000

    added some new tests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -6,8 +6,10 @@ import unittest
 from unittest.mock import patch, Mock, MagicMock, PropertyMock
 from utils import access_nested_map as anm
 from utils import get_json, memoize
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient as GOC
+from requests import HTTPError
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -100,6 +102,58 @@ class TestGithubOrgClient(unittest.TestCase):
         test the has_licence function
         """
         self.assertEqual(GOC.has_license(dic, st), ret)
+
+
+@parameterized_class([
+        {
+            'org_payload': TEST_PAYLOAD[0][0],
+            'repos_payload': TEST_PAYLOAD[0][1],
+            'expected_repos': TEST_PAYLOAD[0][2],
+            'apache2_repos': TEST_PAYLOAD[0][3],
+
+        }
+    ])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    test the functions
+    """
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        set up the class
+        """
+        route_payload = {
+                'https://api.github.com/orgs/google': cls.org_payload,
+                'https://api.github.com/orgs/repos': cls.repos_payload,
+                }
+
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch('requests.get', side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """
+        test the public repo method
+        """
+        self.assertEqual(GOC("google").public_repos(), self.expected_repos,)
+
+    def test_public_repos_with_license(self) -> None:
+        """
+        Test the public repos method wiht a licence
+        """
+        self.assertEqual(GOC("google").public_repos(license="apache-2.0"),
+                         self.apache2_repos,)
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        tear down the class
+        """
+        cls.get_patcher.stop()
 
 
 if __name__ == "__main__":
